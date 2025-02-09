90fbf5d9d0332c138689c5cdbee8c9f092a807a6
Sorelul <sorinake_eu@yahoo.com>
2025-02-09 17:29:44 +0200

    refactor: integrate CitiesContext for city and country components; add BackButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CitiesProvider, useCities } from "./contexts/CitiesContext";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -7,52 +9,29 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import City from "./components/City";
-import { useEffect, useState } from "react";
 import CountriesList from "./components/CountriesList";
 import Form from "./components/Form";
 
-const BASE_URL = "http://localhost:8000";
-
 function App() {
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        async function fetchCities() {
-            try {
-                setIsLoading(true);
-                const response = await fetch(BASE_URL + "/cities");
-                const data = await response.json();
-                setCities(data);
-                setIsLoading(false);
-            } catch (err) {
-                console.error(err);
-                setIsLoading(false);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-
-        fetchCities();
-    }, []);
-
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Homepage />} />
-                <Route path="product" element={<Product />} />
-                <Route path="pricing" element={<Pricing />} />
-                <Route path="login" element={<Login />} />
-                <Route path="app" element={<AppLayout />}>
-                    <Route index element={<Navigate replace to="cities" />} />
-                    <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-                    <Route path="cities/:id" element={<City />} />
-                    <Route path="countries" element={<CountriesList cities={cities} isLoading={isLoading} />} />
-                    <Route path="form" element={<Form />} />
-                </Route>
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </BrowserRouter>
+        <CitiesProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Homepage />} />
+                    <Route path="product" element={<Product />} />
+                    <Route path="pricing" element={<Pricing />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="app" element={<AppLayout />}>
+                        <Route index element={<Navigate replace to="cities" />} />
+                        <Route path="cities" element={<CityList />} />
+                        <Route path="cities/:id" element={<City />} />
+                        <Route path="countries" element={<CountriesList />} />
+                        <Route path="form" element={<Form />} />
+                    </Route>
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </CitiesProvider>
     );
 }
 
---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,61 @@
+import { createContext } from "react";
+import { useEffect, useState, useContext } from "react";
+
+const CitiesContext = createContext();
+const BASE_URL = "http://localhost:8000";
+
+function CitiesProvider({ children }) {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [currentCity, setCurrentCity] = useState({});
+
+    useEffect(() => {
+        async function fetchCities() {
+            try {
+                setIsLoading(true);
+                const response = await fetch(BASE_URL + "/cities");
+                const data = await response.json();
+                setCities(data);
+                setIsLoading(false);
+            } catch (err) {
+                console.error(err);
+                setIsLoading(false);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchCities();
+    }, []);
+
+    async function getCity(id) {
+        try {
+            setIsLoading(true);
+            const response = await fetch(`${BASE_URL}/cities/${id}`);
+            const data = await response.json();
+            setCurrentCity(data);
+            setIsLoading(false);
+        } catch (err) {
+            console.error(err);
+            setIsLoading(false);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <CitiesContext.Provider value={{ cities, isLoading, getCity, currentCity }}>{children}</CitiesContext.Provider>
+    );
+}
+
+function useCities() {
+    const context = useContext(CitiesContext);
+
+    if (!context) {
+        throw new Error("useCities must be used within a CitiesProvider");
+    }
+
+    return context;
+}
+
+export { CitiesProvider, useCities };
